a3aa14148529fa95b0eaac64f5ee747db6de3658
blackdogjet@protonmail.com <blackdogjet@protonmail.com>
2026-04-22 17:32:01 -0500

    B: Added background.

--- draw.go
+++ draw.go
@@ -6,6 +6,28 @@ import (
 
 func draw() {
 	rayLib.BeginDrawing()
+
+	// Set background
+	backgroundSourceRec := rayLib.Rectangle{
+		X:      0,
+		Y:      0,
+		Width:  float32(textBackground.Width),
+		Height: float32(textBackground.Height),
+	}
+
+	backgroundDestinationRec := rayLib.Rectangle{
+		X:      0,
+		Y:      0,
+		Width:  float32(ScreenWidth),
+		Height: float32(ScreenHeight),
+	}
+
+	rayLib.DrawTexturePro(
+		textBackground,
+		backgroundSourceRec,
+		backgroundDestinationRec,
+		rayLib.Vector2{X: 0, Y: 0}, 0, rayLib.White)
+
 	rayLib.ClearBackground(rayLib.Black)
 	rayLib.DrawText("Score: 0", 10, 10, 20, rayLib.Gray)
 	rayLib.EndDrawing()
--- init.go
+++ init.go
@@ -4,11 +4,18 @@ import (
 	rayLib "github.com/gen2brain/raylib-go/raylib"
 )
 
+var (
+	textBackground rayLib.Texture2D
+)
+
 func init() {
 	rayLib.InitWindow(ScreenWidth, ScreenHeight, "Go-Asteroids")
 	rayLib.SetTargetFPS(60)
+
+	textBackground = rayLib.LoadTexture("assets/space_background.png")
 }
 
 func deInit() {
 	rayLib.CloseWindow()
+	rayLib.UnloadTexture(textBackground)
 }
